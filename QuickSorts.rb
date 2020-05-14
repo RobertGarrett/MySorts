4a90a0f35ce2efd727419498022eb64901ea6986
Robert Garrett <robert.garrett712@gmail.com>
2020-05-14 12:42:55 -0500

    Finished QuickSort

--- QuickSorts.rb
+++ QuickSorts.rb
@@ -4,7 +4,6 @@ class QuickSort
     extend Timer
 
     def self.sort(arr)
-        arr = arr.clone
         stack = [[0, arr.length-1]]
 
         while stack.length > 0
@@ -19,45 +18,42 @@ class QuickSort
     def self.partition(arr, low, high)
         pivot = arr[high]
         i = low - 1
-        (low...high).each do |j|
+        (low..high).each do |j|
             if arr[j] <= pivot
                 i += 1
                 arr[i], arr[j] = arr[j], arr[i]
             end
         end
-        arr[i+1], arr[high] = arr[high], arr[i+1]
-        return i+1
+        return i
     end
 end
 
-class MyQuickSort
+class RandomQuickSort
     extend Timer
 
     def self.sort(arr)
-        arr = arr.clone
         stack = [[0, arr.length-1]]
-        print arr.to_s + "\n\n"
         while stack.length > 0
             l, h = *stack.pop
             p = partition(arr, l, h)
-            print "low: #{l}, high: #{h}, avg: #{arr[(h+l)/2]}\n"
-            print arr.to_s + "\n\n"
             stack << [l, p-1] if p-1 > l
             stack << [p+1, h] if p+1 < h
         end
         return arr
     end
 
-    def self.partition(arr, low, high)
-        pivot = arr[(high+low)/2]
-        i = low - 1
-        (low...high).each do |j|
+    def self.partition(arr, l, h)
+        r = rand(l..h)
+        arr[h], arr[r] = arr[r], arr[h]
+
+        pivot = arr[h]
+        i = l - 1
+        (l..h).each do |j|
             if arr[j] <= pivot
                 i += 1
                 arr[i], arr[j] = arr[j], arr[i]
             end
         end
-        arr[i+1], arr[(high+low)/2] = arr[(high+low)/2], arr[i+1]
-        return i+1
+        return i
     end
 end
--- SortTesting.rb
+++ SortTesting.rb
@@ -5,27 +5,27 @@ require 'faker'
 
 
 print "\n_____Random #{"_"*75}\n"
-random = Array.new(10) { rand(0...1000) }
+random = Array.new(100000) { rand(0...100000) }
 Radix.time(random)
 QuickSort.time(random)
-MyQuickSort.time(random)
+RandomQuickSort.time(random)
 ReferenceMergeSort.time(random)
 MyMergeSort.time(random)
 
-# print "\n_____SORTED #{"_"*75}\n"
-# sorted = (1..10).to_a
-# Radix.time(sorted)
-# QuickSort.time(sorted)
-# MyQuickSort.time(sorted)
-# ReferenceMergeSort.time(sorted)
-# MyMergeSort.time(sorted)
-#
-# print "\n_____Reversed #{"_"*75}\n"
-# reversed = sorted.reverse
-# Radix.time(reversed)
-# QuickSort.time(reversed)
-# MyQuickSort.time(reversed)
-# ReferenceMergeSort.time(reversed)
-# MyMergeSort.time(reversed)
+print "\n_____SORTED #{"_"*75}\n"
+sorted = (1..10000).to_a
+Radix.time(sorted)
+QuickSort.time(sorted)
+RandomQuickSort.time(sorted)
+ReferenceMergeSort.time(sorted)
+MyMergeSort.time(sorted)
+
+print "\n_____Reversed #{"_"*75}\n"
+reversed = sorted.reverse
+Radix.time(reversed)
+QuickSort.time(reversed)
+RandomQuickSort.time(reversed)
+ReferenceMergeSort.time(reversed)
+MyMergeSort.time(reversed)
 
 puts puts
