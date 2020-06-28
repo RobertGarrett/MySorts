32ee7f5f08e57c72e0a67b3bbc8d791b9f38731c
Robert Garrett <robert.garrett712@gmail.com>
2020-06-28 16:04:49 -0500

    Optimized Selection Sort

--- Sorts/Selection.rb
+++ Sorts/Selection.rb
@@ -5,13 +5,17 @@ class Selection
     extend Timer
 
     def self.sort(arr)
-        (0...arr.length).each do |i|
-            min = arr[i..-1].each_with_index.inject(i) do |acc, (n, idx)|
-                acc = arr[acc] > n ? idx+i : acc
+        last = arr.length
+        (0...last/2).each do |i|
+            min, max = [i, last-i-1]
+            (i..last-i-1).each do |idx|
+                debugger unless arr[min] && arr[max] && arr[idx]
+                min = idx if arr[min] > arr[idx]
+                max = idx if arr[max] < arr[idx]
             end
-            if min != arr[i]
-                arr[min], arr[i] = arr[i], arr[min]
-            end
+            max = min if i == max
+            arr[min], arr[i] = arr[i], arr[min]
+            arr[max], arr[-i-1] = arr[-i-1], arr[max]
         end
         return arr
     end
