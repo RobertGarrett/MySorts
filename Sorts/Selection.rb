3c6bc6dbeb1127aeb096844410da467c33bbc8e2
Robert Garrett <robert.garrett712@gmail.com>
2020-06-28 16:09:54 -0500

    hotfix

--- Sorts/Selection.rb
+++ Sorts/Selection.rb
@@ -9,12 +9,13 @@ class Selection
         (0...last/2).each do |i|
             min, max = [i, last-i-1]
             (i..last-i-1).each do |idx|
-                debugger unless arr[min] && arr[max] && arr[idx]
                 min = idx if arr[min] > arr[idx]
                 max = idx if arr[max] < arr[idx]
             end
+
+            # Since min is swapped first, the index of max will move to min
             max = min if i == max
-            arr[min], arr[i] = arr[i], arr[min]
+            arr[min],    arr[i] = arr[i],    arr[min]
             arr[max], arr[-i-1] = arr[-i-1], arr[max]
         end
         return arr
